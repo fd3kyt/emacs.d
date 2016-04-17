12f80e8353eac53f7e6675f95440cffcaf38ea4d
fd3kyt <fd3kyt@gmail.com>
2016-04-17 14:50:42 +0800

    startemacs.sh

--- personal/startemacs.sh
+++ personal/startemacs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-env LANG="zh_CN.UTF-8" emacs --daemon --no-desktop
+env LANG="zh_CN.UTF-8" emacs --daemon --no-desktop # --debug-init
 emacsclient -c -n
 sleep 2
 emacsclient -e <<EOF
-"(load-file \"/home/fd3kyt/.emacs.d/kyt/load/init-local-font.el\")"
+"(load-file \"/home/fd3kyt/.emacs.d/personal/load/init-local-font.el\")"
 EOF
