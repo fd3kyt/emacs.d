89f67b87d7c29a9e8e82995abd72507e0bd0b529
fd3kyt <fd3kyt@gmail.com>
2016-04-17 10:54:53 +0800

    改回 startemacs.sh sleep 2

--- personal/startemacs.sh
+++ personal/startemacs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 env LANG="zh_CN.UTF-8" emacs --daemon --no-desktop
-emacsclient -c -n -e <<EOF
+emacsclient -c -n
+sleep 2
+emacsclient -e <<EOF
 "(load-file \"/home/fd3kyt/.emacs.d/kyt/load/init-local-font.el\")"
 EOF
