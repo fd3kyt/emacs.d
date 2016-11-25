990fe19e514853678cbfba1fe01d649e55873827
fd3kyt <fd3kyt@gmail.com>
2016-11-25 16:36:12 +0800

    python, anaconda-mode, clear message

--- personal/load/init-local-python.el
+++ personal/load/init-local-python.el
@@ -10,6 +10,10 @@
 (setq python-shell-interpreter
       "/home/fd3kyt/anaconda3/bin/python3")
 
+(advice-add 'anaconda-mode-create-response-handler
+            :after (lambda (&rest args) (if (window-minibuffer-p)
+                                       (message nil))))
+
 (provide 'init-local-python)
 
 ;;; init-local-python.el ends here
