387e32d90410a9c30220f0d1595cf56a16e04d6a
fd3kyt <fd3kyt@gmail.com>
2016-11-25 21:32:30 +0800

    init-local-git.el

---
+++ personal/load/init-local-git.el
@@ -0,0 +1,11 @@
+;;; init-local-git.el --- Initialize git, magit for me.
+
+;;; Commentary:
+;;
+
+(after-load 'magit
+  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh))
+
+(provide 'init-local-git)
+
+;;; init-local-git.el ends here
