50154f0544e66c6890b8612eccd73217bb81906d
fd3kyt <fd3kyt@gmail.com>
2017-06-14 20:33:42 +0800

    add paredit for org bold and code

---
+++ personal/load/init-local-paredit.el
@@ -0,0 +1,22 @@
+;;; init-local-paredit.el --- Extra initialization for paredit
+
+;;; Commentary:
+;;
+
+;;; Code:
+(after-load 'org
+  (require 'paredit)
+  (defvar org-mode-map)
+  (define-paredit-pair ?\= ?\= "equal")
+  (define-paredit-pair ?\~ ?\~ "tilde")
+  (define-paredit-pair ?\* ?\* "star")
+  (define-paredit-pair ?\+ ?\+ "plus")
+  (define-key org-mode-map (kbd "M-*") 'paredit-wrap-star)
+  (define-key org-mode-map (kbd "M-~") 'paredit-wrap-tilde)
+  (define-key org-mode-map (kbd "M-+") 'paredit-wrap-equal)
+  )
+
+
+(provide 'init-local-paredit)
+
+;;; init-local-paredit.el ends here
