5032eae945bb34d5d6aae23a745324fafdd40551
fd3kyt <fd3kyt@gmail.com>
2017-07-06 08:42:38 +0800

    fix list-packages hang : don't use ssl

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -31,8 +31,9 @@
 
 ;; It seems that my shadowsocks doesn't work well with ssl, so never
 ;; use ssl here.
-(defconst sanityinc/no-ssl (and (memq system-type '(windows-nt ms-dos))
-                                (not (gnutls-available-p))))
+(defconst sanityinc/no-ssl (or t
+                               (and (memq system-type '(windows-nt ms-dos))
+                                    (not (gnutls-available-p)))))
 
 ;;; Also use Melpa for most packages
 (add-to-list 'package-archives
