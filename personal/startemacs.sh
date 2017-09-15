1c58d638c9ff327346e04a265a581ed1de05a1e2
fd3kyt <fd3kyt@gmail.com>
2017-09-15 08:20:22 +0800

    note in startemacs.sh

--- personal/startemacs.sh
+++ personal/startemacs.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #env PATH=~/anaconda3/bin:$PATH LANG="zh_CN.UTF-8" emacs --daemon # --no-desktop # --debug-init
 
+echo "@@@@ Note: if you are debugging emacs,"
+echo "@@@@ run 'emacs' directly instead of using this script."
+
 LOCK_PATH=~/.emacs.d/.emacs.desktop.lock
 if [ -f ${LOCK_PATH} ]; then
     echo "Maybe you need to remove ${LOCK_PATH} manually."
