8dfff21e68bc07fd17bd614ca14239f7f3278c74
fd3kyt <fd3kyt@gmail.com>
2017-09-22 17:48:43 +0800

    hint in startemacs.sh: test-startup.sh

--- personal/startemacs.sh
+++ personal/startemacs.sh
@@ -3,6 +3,7 @@
 
 echo "@@@@ Note: if you are debugging emacs,"
 echo "@@@@ run 'emacs' directly instead of using this script."
+echo "@@@@ even better: ~/.emacs.d/test-startup.sh"
 
 LOCK_PATH=~/.emacs.d/.emacs.desktop.lock
 if [ -f ${LOCK_PATH} ]; then
