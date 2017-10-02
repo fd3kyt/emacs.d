d83013d159c615637ca6996f4b18062e4842c612
fd3kyt <fd3kyt@gmail.com>
2017-10-02 20:53:17 +0800

    add kyt/avy-insert-line-link

--- personal/load/kyt-avy.el
+++ personal/load/kyt-avy.el
@@ -27,6 +27,22 @@ ARG lines can be used."
                   (s-trim str)))))))
 
 
+(declare-function org-insert-link 'org)
+(defvar org-stored-links)
+(defun kyt/avy-insert-line-link ()
+  "Insert a link to a selected line at current position."
+  (interactive)
+  (let ((initial-window (selected-window)))
+    (avy-with avy-copy-line
+      (let ((start (avy--line)))
+        (save-excursion
+          (goto-char start)
+          (call-interactively 'org-store-link)
+          (select-window initial-window)
+          ;; `org-insert-link' will delete the link from `org-stored-links'
+          (org-insert-link nil (car (car org-stored-links))))))))
+
+
 (provide 'kyt-avy)
 
 ;;; kyt-avy.el ends here
