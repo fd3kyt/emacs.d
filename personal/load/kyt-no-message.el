6b25991ca6774493c487a00cfa5bf49f7c128e77
fd3kyt <fd3kyt@gmail.com>
2017-10-10 20:18:05 +0800

    kyt-no-message

---
+++ personal/load/kyt-no-message.el
@@ -0,0 +1,49 @@
+;;; kyt-no-message.el --- Summary
+
+;;; Commentary:
+;;
+
+;;; Code:
+
+
+(defmacro no-message (&rest body)
+  "Eval BODY, with `message' doing nothing."
+  `(cl-letf (((symbol-function 'message)
+              (lambda (&rest args)
+                nil)))
+     (progn ,@body)))
+
+
+(defmacro save-message (&rest body)
+  "Save the message in minibuffer; execute BODY; restore the message."
+  (let ((saved-message-symbol (make-symbol "saved-message")))
+    `(let ((,saved-message-symbol (current-message)))
+       (progn ,@body)
+       (message ,saved-message-symbol))))
+
+
+(defun save-message-advice-around (oldfun rest)
+  "Eval (OLDFUN REST) in `save-message'.
+Usage: (advice-add 'real-auto-save-buffers :around 'save-message-advice-around)"
+  (save-message
+   (apply oldfun rest)))
+
+
+(defun try-out-save-message ()
+  "See how `save-message' works out."
+  (interactive)
+  (message (format (propertize "The message I am staring at (with property)."
+                               'face
+                               'warning)))
+  (save-message
+   (message "Message from the auto save function.")))
+
+
+
+
+
+
+
+(provide 'kyt-no-message)
+
+;;; kyt-no-message.el ends here
