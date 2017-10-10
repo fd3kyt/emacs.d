d74d9437a14daf764462b790bae5ef97a2784b15
fd3kyt <fd3kyt@gmail.com>
2017-10-10 20:19:13 +0800

    less warnings

--- personal/load/init-local-autosave.el
+++ personal/load/init-local-autosave.el
@@ -3,10 +3,10 @@
 ;;; Commentary:
 ;;
 
+;;; Code:
+
 (setq auto-save-interval 300
       auto-save-timeout 120)
-
-;;; Code:
 
 (require-package 'real-auto-save)
 (require 'real-auto-save)
@@ -14,13 +14,19 @@
 
 ;;;; org mode auto save
 (defun kyt/org-setup-autosave ()
+  "Setup autosave for `org-mode'."
   (setq-local real-auto-save-interval 3)
   (real-auto-save-mode))
 (add-hook 'org-mode-hook 'kyt/org-setup-autosave)
 
 (defun kyt/clear-message ()
+  "Clear the message in minibuffer."
   (message nil))
 (advice-add 'real-auto-save-buffers :after 'kyt/clear-message)
+
+(after-load 'whitespace
+  (defvar whitespace-cleanup-mode-preserve-point)
+  (setq whitespace-cleanup-mode-preserve-point t)) ;not working
 
 
 (provide 'init-local-autosave)
