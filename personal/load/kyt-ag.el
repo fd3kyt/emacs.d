f760aad16e6d8afdaca9369885c5d9e949cd0ac4
fd3kyt <fd3kyt@gmail.com>
2017-10-29 17:03:36 +0800

    kyt-ag add a lot

--- personal/load/kyt-ag.el
+++ personal/load/kyt-ag.el
@@ -62,7 +62,33 @@ and `man' of ag: 'ag [options] pattern [path ...]'"
   (let ((final-options (append kyt-ag-base-options options)))
     (kyt-ag--run final-options pattern paths)))
 
-(kyt-ag-run nil "peco" (list "/home/fd3kyt/Documents"))
+(kyt-ag-run '("-A" "3") "peco" (list "/home/fd3kyt/Documents"))
+
+
+(defun shlex-split (string)
+  "Split STRING with whitespace, except the quoted parts.
+
+Note: when we type a double quote in `read-string', we will get a
+escaped double quote(\").  But this doesn't happen on single
+quotes."
+  (split-string-and-unquote (s-replace "'" "\"" string)))
+
+
+(defvar kyt-ag-option-history nil
+  "History of `kyt-ag-run-string' option.")
+
+(defvar kyt-ag-pattern-history nil
+  "History of `kyt-ag-run-string' pattern.")
+
+
+(defun kyt-ag-run-string (option-string pattern path-string)
+  "Read OPTION-STRING, PATTERN, PATH-STRING from minibuffer."
+  (interactive (list (read-string "Options: " nil 'kyt-ag-option-history)
+                     (read-string "Pattern: " nil 'kyt-ag-pattern-history)
+                     (read-directory-name "Directory: ")))
+  (kyt-ag-run (shlex-split option-string)
+              pattern
+              (list path-string)))
 
 
 (magit-define-popup kyt-try-magit-popup-8-popup
@@ -84,9 +110,30 @@ and `man' of ag: 'ag [options] pattern [path ...]'"
   (prin1 magit-current-popup-args))
 
 
+(magit-define-popup kyt-ag-magit-popup-1-popup
+  "Popup console for `kyt-ag' options."
+  'kyt-ag-option-popup
+  :actions '("Section 1"
+             (?i "Initialize defaults" magit-gitflow-init)
+             (?f "Feature prefix"      magit-gitflow-init-feature)
+             "Section 2"
+             (?u "Run in popup" run-in-popup))
+  :switches '("File type"
+              (?t "Force reinitialization" "--force")
+              (?k "Kaka" "-k"))
+  :options '((?t "Timeout" "--time=" read-number))
+  :default-arguments '("-k" "--time=200"))
+
+
+(defun run-in-popup ()
+  (interactive)
+  (prin1 magit-current-popup-args))
+
+
 (ag/get-supported-types)
 
 
+(read-extended-command)
 
 ;; todo ag/get-supported-types, ag/read-file-type
 
