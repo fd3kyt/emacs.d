eaca06e4b6cd425981bb3b7d481be2f221c4fdf9
fd3kyt <fd3kyt@gmail.com>
2017-11-07 22:19:51 +0800

    use marker

--- personal/load/kyt-org.el
+++ personal/load/kyt-org.el
@@ -19,26 +19,27 @@
     (mapc 'print
           (org-element-map tree 'headline
             (lambda (hl) (when (> (length (org-element-property :tags hl)) 1)
-                      (substring-no-properties
-                       (car (org-element-property :title hl)))))))))
+                       (substring-no-properties
+                        (car (org-element-property :title hl)))))))))
 
 
 ;; go to :begin and org-set-property doesn't work. org-set-property
 ;; will add new lines. the data of point is out of date.
+
+;; Use markers, ok now.
 (defun kyt/test-org-element-add-property ()
   (interactive)
   (let* ((tree (org-element-parse-buffer))
          (headlines (org-element-map tree 'headline
                       (lambda (hl) (when (> (length (org-element-property :tags hl)) 0)
-                                hl))))
+                                 hl))))
          (markers (mapcar (lambda (hl) (progn
-                                    (goto-char (org-element-property :begin hl))
-                                    (point-marker)))
+                                     (goto-char (org-element-property :begin hl))
+                                     (point-marker)))
                           headlines)))
-    (cl-loop for m in markers
-             do (progn
-                  (goto-char m)
-                  (org-set-property "id" "100")))))
+    (dolist (m markers)
+      (goto-char m)
+      (org-set-property "id" "234"))))
 
 
 (org-sparse-tree)
