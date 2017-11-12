206fb476f7d2f640251d8bb01fbc3f9d42815997
fd3kyt <fd3kyt@gmail.com>
2017-11-12 16:04:34 +0800

    use with-eval-after-load

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -269,23 +269,23 @@ state."
 
 ;; image+
 ;; https://github.com/mhayashi1120/Emacs-imagex
-(eval-after-load 'image '(require 'image+))
-(eval-after-load 'image+ '(imagex-global-sticky-mode 1))
-(eval-after-load 'image+ '(imagex-auto-adjust-mode 1))
-(eval-after-load 'image+ '(setq imagex-auto-adjust-threshold 1))
-(eval-after-load 'image+
-  `(when (require 'hydra nil t)
-     (defhydra imagex-sticky-binding (global-map "C-x C-l")
-       "Manipulating Image"
-       ("+" imagex-sticky-zoom-in "zoom in")
-       ("-" imagex-sticky-zoom-out "zoom out")
-       ("M" imagex-sticky-maximize "maximize")
-       ("O" imagex-sticky-restore-original "restore original")
-       ("S" imagex-sticky-save-image "save file")
-       ("r" imagex-sticky-rotate-right "rotate right")
-       ("l" imagex-sticky-rotate-left "rotate left"))))
+(with-eval-after-load 'image
+  (require 'image+)
+  (imagex-global-sticky-mode 1)
+  (imagex-auto-adjust-mode 1)
+  (setq imagex-auto-adjust-threshold 1)
+  (when (require 'hydra nil t)
+    (defhydra imagex-sticky-binding (global-map "C-x C-l")
+      "Manipulating Image"
+      ("+" imagex-sticky-zoom-in "zoom in")
+      ("-" imagex-sticky-zoom-out "zoom out")
+      ("M" imagex-sticky-maximize "maximize")
+      ("O" imagex-sticky-restore-original "restore original")
+      ("S" imagex-sticky-save-image "save file")
+      ("r" imagex-sticky-rotate-right "rotate right")
+      ("l" imagex-sticky-rotate-left "rotate left"))))
 
-(eval-after-load 'ag
+(with-eval-after-load 'ag
   (add-to-list 'ag-arguments "-U"))
 
 (yas-reload-all)
