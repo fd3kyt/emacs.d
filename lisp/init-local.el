4e72101eaf07e67662b92ad412aa39cdf3922f0c
fd3kyt <fd3kyt@gmail.com>
2017-11-13 08:38:46 +0800

    flycheck-idle-change-delay

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -108,7 +108,7 @@
 (with-eval-after-load 'flycheck
   ;; shows errors under point in pos-tip popups
   (flycheck-pos-tip-mode)
-  (setq flycheck-idle-change-delay 0.2))
+  (setq flycheck-idle-change-delay 0.3))
 
 ;; System locale to use for formatting time values.
 (setq system-time-locale "C")
