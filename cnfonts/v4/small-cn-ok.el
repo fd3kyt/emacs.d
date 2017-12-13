e5f5e20a45ff0f3a34625b4e0887f0a1390e7a32
fd3kyt <fd3kyt@gmail.com>
2017-12-13 09:54:38 +0800

    font

--- cnfonts/v4/small-cn-ok.el
+++ cnfonts/v4/small-cn-ok.el
@@ -4,7 +4,7 @@
 ;;; `cnfonts-insert-fontname’ 来选择一个 *可用* 字体，然后在当前光标处插入其字体名称。
 (setq cnfonts--custom-set-fontnames
       '(
-        ("Inconsolata" "Inconsolata LGC" "DejaVu Sans Mono" "Monaco" "Consolas" "Droid Sans Mono" "PragmataPro" "Courier" "Courier New" "Ubuntu Mono" "Liberation Mono" "MonacoB" "MonacoB2" "MonacoBSemi" "Droid Sans Mono Pro" "Source Code Pro" "Lucida Console" "Envy Code R" "Andale Mono" "Lucida Sans Typewriter" "monoOne" "Lucida Typewriter" "Panic Sans" "Hack" "Bitstream Vera Sans Mono" "HyperFont" "PT Mono" "Ti92Pluspc" "Excalibur Monospace" "Menlof" "Cousine" "Fira Mono" "Lekton" "M+ 1mn" "BPmono" "Free Mono" "Anonymous Pro" "ProFont" "ProFontWindows" "Latin Modern Mono" "Code 2002" "ProggyCleanTT" "ProggyTinyTT")
+        ("Consolas" "Inconsolata" "DejaVu Sans Mono" "Inconsolata LGC" "Monaco" "Droid Sans Mono" "PragmataPro" "Courier" "Courier New" "Ubuntu Mono" "Liberation Mono" "MonacoB" "MonacoB2" "MonacoBSemi" "Droid Sans Mono Pro" "Source Code Pro" "Lucida Console" "Envy Code R" "Andale Mono" "Lucida Sans Typewriter" "monoOne" "Lucida Typewriter" "Panic Sans" "Hack" "Bitstream Vera Sans Mono" "HyperFont" "PT Mono" "Ti92Pluspc" "Excalibur Monospace" "Menlof" "Cousine" "Fira Mono" "Lekton" "M+ 1mn" "BPmono" "Free Mono" "Anonymous Pro" "ProFont" "ProFontWindows" "Latin Modern Mono" "Code 2002" "ProggyCleanTT" "ProggyTinyTT")
         ("Microsoft Yahei" "Hiragino Sans GB" "微软雅黑" "Noto Sans S Chinese Regular" "Microsoft_Yahei" "Ubuntu Mono" "文泉驿等宽微米黑" "文泉驿等宽正黑" "黑体" "文泉驿正黑" "文泉驿点阵正黑" "SimHei" "SimSun" "NSimSun" "FangSong" "KaiTi" "FangSong_GB2312" "KaiTi_GB2312" "LiSu" "YouYuan" "新宋体" "宋体" "楷体_GB2312" "仿宋_GB2312" "幼圆" "隶书" "STXihei" "STKaiti" "STSong" "STZhongsong" "STFangsong" "FZShuTi" "FZYaoti" "STCaiyun" "STHupo" "STLiti" "STXingkai" "STXinwei" "方正姚体" "方正舒体" "方正粗圆_GBK" "华文仿宋" "华文中宋" "华文彩云" "华文新魏" "华文细黑" "华文行楷" "DejaVu Sans Mono")
         ("HanaMinB" "DejaVu Sans Mono" "SimSun-ExtB" "MingLiU-ExtB" "PMingLiU-ExtB" "MingLiU_HKSCS-ExtB")
         ))
@@ -17,9 +17,9 @@
         (9    10.5 10.5)
         (10   10.0 12.0)
         (11.5 8.0  13.5)
-        (12.5 10.5 16.0)
-        (14   11.5 16.5)
-        (15   11.5 18.0)
+        (12.5 9.0  16.0)
+        (14   10.5 16.5)
+        (15   10.5 18.0)
         (16   11.5 19.5)
         (18   14.5 21.0)
         (20   24.0 24.0)
