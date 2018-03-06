a2e073e615feb1878bfc31dec22d296708f539c1
fd3kyt <fd3kyt@gmail.com>
2018-03-06 16:36:27 +0800

    add init-local-koe

---
+++ personal/load/init-local-koe.el
@@ -0,0 +1,13 @@
+;;; init-local-koe.el --- Summary -*- lexical-binding: t -*-
+
+;;; Commentary:
+;;
+
+;;; Code:
+
+
+(add-to-list 'load-path "/home/XXD/Projects/org2anki/anki-org/")
+(require 'koe)
+(provide 'init-local-koe)
+
+;;; init-local-koe.el ends here
