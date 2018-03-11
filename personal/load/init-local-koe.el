b5c9346f2543f8dfd8b47abbfcb2311b9f961619
fd3kyt <fd3kyt@gmail.com>
2018-03-11 11:16:42 +0800

    koe deck alias

--- personal/load/init-local-koe.el
+++ personal/load/init-local-koe.el
@@ -8,6 +8,15 @@
 
 (add-to-list 'load-path "/home/XXD/Projects/org2anki/anki-org/")
 (require 'koe)
+
+(defvar koe-anki-deck-alias-under-cs nil)
+(setq koe-anki-deck-alias-under-cs
+      '("cpp" "emacs" "python" "sql" "project" "leetcode"))
+
+(dolist (name koe-anki-deck-alias-under-cs)
+  (add-to-list 'koe-anki-deck-alias (cons name (s-concat "cs::" name))))
+
+
 (provide 'init-local-koe)
 
 ;;; init-local-koe.el ends here
