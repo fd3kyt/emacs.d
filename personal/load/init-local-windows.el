a833aa0393d49b4c32619ea72e6cfd7f98989b5f
fd3kyt <fd3kyt@gmail.com>
2020-06-13 20:50:37 +0800

    add init-local-windows

---
+++ personal/load/init-local-windows.el
@@ -0,0 +1,12 @@
+;;; init-local-windows.el --- Summary -*- lexical-binding: t -*-
+
+;;; Commentary:
+;;
+
+;;; Code:
+
+(setq w32-recognize-altgr nil)
+
+(provide 'init-local-windows)
+
+;;; init-local-windows.el ends here
