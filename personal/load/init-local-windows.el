224d30dc640e2761b7e812b2fb12517dd2bd7999
fd3kyt <fd3kyt@gmail.com>
2020-07-24 12:07:37 +0800

    Win: try to maximize at startup

--- personal/load/init-local-windows.el
+++ personal/load/init-local-windows.el
@@ -7,6 +7,10 @@
 
 (setq w32-recognize-altgr nil)
 
+;;; https://emacs.stackexchange.com/questions/2999/how-to-maximize-my-emacs-frame-on-start-up
+;;; doesn't work?
+;; (add-to-list 'default-frame-alist '(fullscreen . maximized))
+
 (provide 'init-local-windows)
 
 ;;; init-local-windows.el ends here
