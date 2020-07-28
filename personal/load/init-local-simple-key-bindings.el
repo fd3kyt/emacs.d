f987c11054b09659544a115fcf470be484d6757e
fd3kyt <fd3kyt@gmail.com>
2020-07-28 10:32:50 +0800

    bind kyt/avy-copy-line-here to C-c L

--- personal/load/init-local-simple-key-bindings.el
+++ personal/load/init-local-simple-key-bindings.el
@@ -14,6 +14,7 @@
 (global-set-key (kbd "C-:") 'avy-goto-char)
 (global-set-key (kbd "C-M-z") 'avy-goto-char-in-line)
 (global-set-key (kbd "C-S-y") 'kyt/avy-copy-line-here)
+(global-set-key (kbd "C-c L") 'kyt/avy-insert-line-link)
 
 (global-set-key (kbd "C-j") 'kyt/new-line)
 (global-set-key (kbd "C-S-L") 'move-to-window-line-top-bottom)
