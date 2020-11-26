781c75b6eb541c5b60dcf905f9b2f4851f80a250
fd3kyt <fd3kyt@gmail.com>
2020-11-26 22:51:58 +0800

    add keybindings for magit-section-show-level-1/2/3/4-all

--- personal/load/init-local-git.el
+++ personal/load/init-local-git.el
@@ -7,9 +7,6 @@
 (declare-function 'require-package 'init-elpa)
 (declare-function 'after-load 'subr)
 
-(after-load 'magit
-  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh))
-
 ;; https://github.com/magit/magit/issues/1275
 (defadvice magit-start-git (around lang-en_US activate)
   "Set LANG to en_US."
@@ -17,15 +14,28 @@
     (setenv "LANG" "en_US")
     ad-do-it))
 
-(require-package 'magit-gitflow)
+(after-load 'magit
+  ;; update the highlights of uncommitted changes in fridge
+  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
 
+  (defvar magit-log-margin)
+  (setq magit-log-margin '(t "%y-%m-%d %H:%M" magit-log-margin-width t 18))
+
+  (defvar magit-status-mode-map)
+  (define-key magit-status-mode-map (kbd "C-c 1") 'magit-section-show-level-1-all)
+  (define-key magit-status-mode-map (kbd "C-c 2") 'magit-section-show-level-2-all)
+  (define-key magit-status-mode-map (kbd "C-c 3") 'magit-section-show-level-3-all)
+  (define-key magit-status-mode-map (kbd "C-c 4") 'magit-section-show-level-4-all)
+  )
+
+
+;;; magit-gitflow
+(require-package 'magit-gitflow)
 (after-load 'magit
   (require 'magit-gitflow)
   (defvar magit-gitflow-mode-map)
   (define-key magit-gitflow-mode-map (kbd "C-f") nil)
-  (define-key magit-gitflow-mode-map (kbd "C-c f") 'magit-gitflow-popup)
-  (defvar magit-log-margin)
-  (setq magit-log-margin '(t "%y-%m-%d %H:%M" magit-log-margin-width t 18)))
+  (define-key magit-gitflow-mode-map (kbd "C-c f") 'magit-gitflow-popup))
 
 (provide 'init-local-git)
 
