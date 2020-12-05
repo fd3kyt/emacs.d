5b3c16136f257b6e2cef6f6c6de512dc884ec29c
fd3kyt <fd3kyt@gmail.com>
2020-12-05 20:48:38 +0800

    comment in kyt-no-message

--- personal/init/kyt-no-message.el
+++ personal/init/kyt-no-message.el
@@ -1,7 +1,9 @@
 ;;; kyt-no-message.el --- Summary
 
 ;;; Commentary:
+;; utilities for suppressing message.
 ;;
+;; why not just use `inhibit-message': will clear current message
 
 ;;; Code:
 
@@ -16,7 +18,6 @@
                 nil)))
      (progn ,@body)))
 
-
 (defmacro save-message (&rest body)
   "Save the message in minibuffer; execute BODY; restore the message."
   (let ((saved-message-symbol (make-symbol "saved-message")))
@@ -25,15 +26,13 @@
        (message ,saved-message-symbol)
        return)))
 
-
 (defun save-message-advice-around (oldfun rest)
   "Eval (OLDFUN REST) in `save-message'.
 Usage: (advice-add 'real-auto-save-buffers :around 'save-message-advice-around)"
   (save-message
    (apply oldfun rest)))
 
-
-(defun try-out-save-message ()
+(defun --try-out-save-message ()
   "See how `save-message' works out."
   (interactive)
   (message (format (propertize "The message I am staring at (with property)."
@@ -42,11 +41,13 @@ Usage: (advice-add 'real-auto-save-buffers :around 'save-message-advice-around)"
   (save-message
    (message "Message from the auto save function.")))
 
-
-
-
-
-
+(defun --try-inhibit-message ()
+  "Try to use `inhibit-message'."
+  (interactive)
+  (message "Old message")
+  (let ((inhibit-message t))
+    ;; will still clear current message
+    (message "New message")))
 
 (provide 'kyt-no-message)
 
