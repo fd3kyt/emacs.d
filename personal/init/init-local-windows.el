78507e3eea24ae61fea2848dae38bdaee5c67f5f
fd3kyt <fd3kyt@gmail.com>
2021-04-22 18:20:33 +0800

    fix windows shell command garbled

--- personal/init/init-local-windows.el
+++ personal/init/init-local-windows.el
@@ -38,6 +38,23 @@
 ;;  'fcitx/im-test-if-turned-on-command
 ;;  "d:/nutstore/diy/Windows/ime-switch/switch-language.exe =00000804")
 
+;;; fix windows shell command garbled:
+;; beta way: Windows region setting: use utf-8 for global support
+(defun fix-shell-command-garbled-in-windows (fun &rest args)
+  "Call FUN with ARGS."
+  (let ((coding-system-for-read 'gbk)
+        (coding-system-for-write 'gbk))
+    (apply fun args)))
+(advice-add 'shell-command :around 'fix-shell-command-garbled-in-windows)
+(defun fix-shell-mode-garbled-in-windows ()
+  "Fix shell garbled in Windows."
+  (set-process-coding-system (get-buffer-process (current-buffer)) 'gbk 'gbk))
+(add-hook 'shell-mode-hook 'fix-shell-mode-garbled-in-windows)
+
+;;; use powershell as default shell
+;; (setq shell-file-name "powershell")
+;; XXX will break M-x shell, use eshell instead; will break ag
+
 (provide 'init-local-windows)
 
 ;;; init-local-windows.el ends here
